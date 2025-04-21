a36bcbd5a943d1fdab68d11fdde28e7783893309
abbydise <abbydise9@gmail.com>
2025-04-21 14:13:49 -0400

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import React from "react";
-import NavBar from "./components/NavBar";
 import MainPage from "./pages/MainPage";
 import ProjectsPage from "./pages/ProjectsPage";
 import ProjectCard from "./components/ProjectCard";
@@ -8,7 +7,7 @@ import ProjectCard from "./components/ProjectCard";
 function App() {
 	return (
 		<div>
-			<NavBar />
+			<MainPage />
 		</div>
 	);
 }
