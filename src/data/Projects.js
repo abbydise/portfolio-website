9d2471c2887814746709b5a17e43f817f3cd0e30
abbydise <abbydise9@gmail.com>
2025-05-08 13:20:34 -0400

    added a data js file to store projects

---
+++ src/data/Projects.js
@@ -0,0 +1,22 @@
+export const projects = {
+	Flutters: {
+		title: "Flutters",
+		description:
+			"Flutters is a web application which allows users to find activities to do in their area based on their preferences.",
+		technologies: [
+			"SvelteKit",
+			"Tailwind CSS",
+			"JavaScript",
+			"PostgreSQL",
+		],
+		github: null,
+		website: "https://flutters-topaz.vercel.app/",
+	},
+	Portfolio: {
+		title: "Personal Portfolio Website",
+		description: "This website!",
+		technologies: ["React", "Tailwind CSS"],
+		github: "https://github.com/abbydise/portfolio-website",
+		website: "https://abbydise.com",
+	},
+};
