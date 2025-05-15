3d716ef8fd81df17c2d5864a46197230630b5989
abbydise <abbydise9@gmail.com>
2025-05-15 16:29:36 -0400

    added contact form attached to my email

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactForm = () => {
-	return <div>ContactForm</div>;
+	const form = useRef();
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(
+				import.meta.env.VITE_EMAILJS_SERVICE_ID,
+				import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+				form.current,
+				{ publicKey: import.meta.env.VITE_EMAILJS_KEY }
+			)
+			.then(
+				() => {
+					console.log("SUCCESS!");
+				},
+				(error) => {
+					console.log("FAILED...", error.text);
+				}
+			);
+	};
+	return (
+		<form
+			ref={form}
+			onSubmit={sendEmail}
+		>
+			<legend>Full Name</legend>
+			<input
+				type="text"
+				name="fullName"
+				className="border"
+			/>
+			<legend>Email</legend>
+			<input
+				type="email"
+				name="email"
+				className="border"
+			/>
+			<legend>Message</legend>
+			<textarea
+				name="message"
+				className="border"
+			/>
+			<input
+				type="submit"
+				value="Send"
+				className="border rounded-2xl p-2 ml-5"
+			/>
+		</form>
+	);
 };
 
 export default ContactForm;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
+import ContactForm from "./ContactForm";
+
 const Footer = () => {
 	return (
 		<div
 			id="contact"
-			className="text-left mt-75"
+			className="text-left mt-20"
 		>
 			<h1 className="text-3xl">Contact Me!</h1>
+			<ContactForm />
 		</div>
 	);
 };
