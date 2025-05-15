092a7477cc51d6fd57ef541f1c13f8fe5cdab24a
abbydise <abbydise9@gmail.com>
2025-05-15 15:59:13 -0400

    implemented a back button for projects page

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 import ProjectBlogCard from "../components/ProjectBlogCard";
 
 import { projects } from "../data/projects";
@@ -9,7 +11,12 @@ const ProjectsPage = () => {
 			id="projects"
 			className="text-left mt-20"
 		>
-			<h1 className="text-3xl">All Projects</h1>
+			<div className="mb-10">
+				<Link to="/">
+					<img src="arrow.png" /> Go Back to Home Page
+				</Link>
+			</div>
+			<h1 className="text-3xl text-center">All Projects</h1>
 			<div className="grid gap-5 mt-10">
 				<ProjectBlogCard project={projects.Flutters} />
 				<ProjectBlogCard project={projects.Portfolio} />
