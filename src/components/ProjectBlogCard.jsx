669725bc698cad9745ede8d637756a794b8ebfa2
abbydise <abbydise9@gmail.com>
2025-05-15 15:52:22 -0400

    added basic blog style card to projects page

--- src/components/ProjectBlogCard.jsx
+++ src/components/ProjectBlogCard.jsx
@@ -1,9 +1,49 @@
 import React from "react";
 
-import { projects } from "../data/projects";
-
-const ProjectBlogCard = () => {
-	return <div>ProjectBlogCard</div>;
+const ProjectBlogCard = ({ project }) => {
+	return (
+		<div className="border rounded-lg p-5 relative">
+			<h1 className="text-2xl underline">{project.title}</h1>
+			<p className="mt-5 mb-5">{project.long_description}</p>
+			<div className="mt-5 flex gap-1 mb-10">
+				{project.technologies.map((item, index) => (
+					<p id={index}>{item}</p>
+				))}
+			</div>
+			<div className="flex absolute right-0 bottom-0 m-5">
+				{project.github && (
+					<div className="float-left">
+						<img
+							src="github.svg"
+							className="w-6 h-6 float-left mr-2"
+						/>
+						<a
+							href={project.github}
+							target="_blank"
+							className="mr-2"
+						>
+							GitHub
+						</a>
+					</div>
+				)}
+				{project.website && (
+					<div>
+						<img
+							src="link.png"
+							className="w-5 h-5 float-left"
+						/>
+						<a
+							href={project.website}
+							target="_blank"
+							className="ml-1"
+						>
+							Live Demo
+						</a>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default ProjectBlogCard;
--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import ProjectBlogCard from "../components/ProjectBlogCard";
 
-import { projects } from "../data/Projects";
+import { projects } from "../data/projects";
 
 const ProjectsPage = () => {
-	return <h1>{projects.Portfolio.title}</h1>;
+	return (
+		<div
+			id="projects"
+			className="text-left mt-20"
+		>
+			<h1 className="text-3xl">All Projects</h1>
+			<div className="grid gap-5 mt-10">
+				<ProjectBlogCard project={projects.Flutters} />
+				<ProjectBlogCard project={projects.Portfolio} />
+			</div>
+		</div>
+	);
 };
 
 export default ProjectsPage;
