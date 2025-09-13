39a0f9a48ec30607d5f1f4e2b321e54bfe534052
abbydise <abbydise9@gmail.com>
2025-09-13 18:21:34 -0400

    populated skills data

---
+++ src/data/Skills.js
@@ -0,0 +1,42 @@
+export const databases = [
+	{ name: "MongoDB", icon: "skills/databases/mongodb.svg" },
+	{ name: "PostgreSQL", icon: "skills/databases/postgresql.svg" },
+	{ name: "MySQL", icon: "skills/databases/sql.svg" },
+];
+
+export const frameworks = [
+	{ name: "Express.js", icon: "skills/frameworks/expressjs.svg" },
+	{ name: "Node.js", icon: "skills.frameworks/nodejs.svg" },
+	{ name: "React", icon: "skills/frameworks/react.svg" },
+	{ name: "Spring Boot", icon: "skills/frameworks/spring.svg" },
+	{ name: "Tailwind CSS", icon: "skills/frameworks/tailwindcss.svg" },
+];
+
+export const languages = [
+	{ name: "CSS", icon: "skills/languages/css.svg" },
+	{ name: "HTML", icon: "skills/languages/html.svg" },
+	{ name: "Java", icon: "skills/languages/java.svg" },
+	{ name: "JavaScript", icon: "skills/languages/javascript.svg" },
+	{ name: "Kotlin", icon: "skills/languages/kotlin.svg" },
+	{ name: "Python", icon: "skills/languages/python.svg" },
+	{ name: "TypeScript", icon: "skills/languages/typescript.svg" },
+];
+
+export const os = [
+	{ name: "MacOS", icon: "skills/os/macOS.svg" },
+	{ name: "Windows", icon: "skills/os/windows.svg" },
+];
+
+export const tools = [
+	{ name: "Amazon Web Services (AWS)", icon: "skills/tools/AWS.svg" },
+	{ name: "Command Line Interface (CLI)", icon: "skills/tools/cli.svg" },
+	{ name: "Docker", icon: "skills/tools/docker.svg" },
+	{ name: "Figma", icon: "skills/tools/figma.svg" },
+	{ name: "Git", icon: "skills/tools/git.svg" },
+	{ name: "GitHub", icon: "skills/tools/github.svg" },
+	{ name: "GitLab", icon: "skills/tools/gitlab.svg" },
+	{ name: "IntelliJ IDEA", icon: "skills/tools/intellij.svg" },
+	{ name: "Jira", icon: "skills/tools/jira.svg" },
+	{ name: "Postman", icon: "skills/tools/postman.svg" },
+	{ name: "Visual Studio Code (VSCode)", icon: "skills/tools/vscode,svg" },
+];
