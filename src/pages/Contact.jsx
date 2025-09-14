4ef57b346c8417d2398abda619d7ab8eecd26028
abbydise <abbydise9@gmail.com>
2025-09-14 17:47:05 -0400

    added contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,7 +7,59 @@ const Contact = () => {
 	const sendEmail = (e) => {
 		e.preventDefault();
 
-		emailjs.sendForm();
+		emailjs
+			.sendForm(
+				import.meta.env.VITE_SERVICE_ID,
+				import.meta.env.VITE_TEMPLATE_ID,
+				form.current,
+				{
+					publicKey: import.meta.env.VITE_PUBLIC_KEY,
+				}
+			)
+			.then(
+				() => {
+					console.log("SUCCESS!");
+					form.current.reset();
+				},
+				(error) => {
+					console.log("FAILED...", error);
+				}
+			);
 	};
+
+	return (
+		<div className="w-150">
+			<form
+				ref={form}
+				onSubmit={sendEmail}
+			>
+				<input
+					type="text"
+					name="user_name"
+					placeholder="Full Name"
+					className="border rounded-xl w-50 h-10 p-2"
+					required
+				/>
+				<input
+					type="email"
+					name="user_email"
+					placeholder="Email Address"
+					className="border rounded-xl w-50 h-10 p-2 ml-5"
+					required
+				/>
+				<textarea
+					name="message"
+					placeholder="Your Message"
+					className="border rounded-xl w-150 h-100 p-2 mt-5"
+					required
+				/>
+				<input
+					type="submit"
+					value="Send"
+					className="flex border rounded-xl w-20 mt-5 justify-self-end"
+				/>
+			</form>
+		</div>
+	);
 };
 export default Contact;
